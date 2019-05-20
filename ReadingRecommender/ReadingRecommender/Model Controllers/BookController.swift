c9559344aeee665c4317ca125eee213a22b95978
Christopher Aronson <christopheraronson@icloud.com>
2019-05-20 16:04:27 -0700

    Created Book model and controller

---
+++ ReadingRecommender/ReadingRecommender/Model Controllers/BookController.swift
@@ -0,0 +1,15 @@
+//
+//  BookController.swift
+//  ReadingRecommender
+//
+//  Created by Christopher Aronson on 5/20/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books = Book(title: "Alice in Wonderland", description: """
+                                                                Alice is a fictional character and protagonist of Lewis Carroll's children's novel Alice's Adventures in Wonderland (1865) and its sequel, Through the Looking-Glass (1871). A child in the mid-Victorian era, Alice unintentionally goes on an underground adventure after accidentally falling down a rabbit hole into Wonderland; in the sequel, she steps through a mirror into an alternative world.
+                                                                """)
+}
---
+++ ReadingRecommender/ReadingRecommender/Models/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  ReadingRecommender
+//
+//  Created by Christopher Aronson on 5/20/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    let title: String
+    let description: String
+}
