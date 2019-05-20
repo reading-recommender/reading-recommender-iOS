988e3c69e85174763599476eff559fdc310c1a00
Christopher Aronson <christopheraronson@icloud.com>
2019-05-20 13:50:26 -0700

    Created some test Data

---
+++ ReadingRecommender/ReadingRecommender/Model Controllers/UserController.swift
@@ -0,0 +1,13 @@
+//
+//  UserController.swift
+//  ReadingRecommender
+//
+//  Created by Christopher Aronson on 5/20/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    let testUser = User(username: "christopher", password: "test", token: "N/A")
+}
