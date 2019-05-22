1ca0135eb2bd31b65dfd8e74990c44b76bf84f53
Christopher Aronson <christopheraronson@icloud.com>
2019-05-21 21:36:10 -0700

    Fixed Home button on Books View

--- ReadingRecommender/ReadingRecommender/View Controllers/BookViewController.swift
+++ ReadingRecommender/ReadingRecommender/View Controllers/BookViewController.swift
@@ -14,26 +14,19 @@ class BookViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         updateView()
-        styleDoneButton()
     }
     
     private func updateView() {
         titleLabel.text = bookController.books.title
         descriptionLabel.text = bookController.books.description
     }
-    
-    private func styleDoneButton() {
-        Appearance.styleNavigation(button: doneButton)
-    }
 
-    @IBAction func doneButtonTapped(_ sender: Any) {
-        performSegue(withIdentifier: "unwindToHome", sender: nil)
+    @IBAction func homeButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "goHome", sender: nil)
     }
-
 }
